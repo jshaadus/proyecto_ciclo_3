077b026a38eff15fdd0294489ebd4bce64ab1210
aatellezve <95355145+aatellezve@users.noreply.github.com>
2022-09-21 23:06:32 -0500

    Add files via upload

---
+++ proyecto_ciclo_3/src/index.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+'''
+# Creating simple Routes 
+@app.route('/')
+def test():
+    return "Home Page"
+
+
+@app.route('/test/about/')
+def about_test():
+    return "About Page"
+
+'''
+
+# rutas para renderizar
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/administrador', strict_slashes=False)
+def administrador():
+    return render_template("administrador.html")
+
+
+@app.route('/docentes', strict_slashes=False)
+def docentes():
+    return render_template("docentes.html")
+
+
+@app.route('/estudiantes', strict_slashes=False)
+def estudiantes():
+    return render_template("estudiantes.html")
+
+@app.route('/notasEstudiante', strict_slashes=False)
+def notasEstudiante():
+    return render_template("notasEstudiante.html")    
+
+
+# Make sure this we are executing this file
+if __name__ == '__main__':
+    app.run(debug=True)
